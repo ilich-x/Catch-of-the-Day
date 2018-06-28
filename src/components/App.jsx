7f85bb27f6459059e5713ca29310a3791903722c
Ilya Nazarov <nazarov@uchi.ru>
2018-06-28 11:54:18 +0300

    remove from odred btn

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,33 +10,33 @@ class App extends React.Component {
   state = {
     fishes: {},
     order: {}
-  }
+  };
   componentDidMount() {
     const { params } = this.props.match;
-    const localStorageOrder = localStorage.getItem(params.storeId)
+    const localStorageOrder = localStorage.getItem(params.storeId);
     if (localStorageOrder) {
       this.setState({
         order: JSON.parse(localStorageOrder)
-      })
+      });
     }
     this.ref = base.syncState(`${params.storeId}/fishes`, {
       context: this,
       state: 'fishes'
-    }); 
+    });
   }
 
   componentDidUpdate() {
     localStorage.setItem(
       this.props.match.params.storeId,
       JSON.stringify(this.state.order)
-    )
+    );
   }
 
   componentWillUnmount() {
     base.removeBinding(this.ref);
   }
 
-  addFish = (fish) => {
+  addFish = fish => {
     // copy of the existing state
     const fishes = { ...this.state.fishes };
     // new one
@@ -45,49 +45,62 @@ class App extends React.Component {
     this.setState({
       fishes
     });
-  }
+  };
 
   updateFish = (key, updatedFish) => {
     const fishes = { ...this.state.fishes };
     fishes[key] = updatedFish;
-    this.setState({ fishes })
-  }
+    this.setState({ fishes });
+  };
 
-  deleteFish = (key) => {
+  deleteFish = key => {
     const fishes = { ...this.state.fishes };
     //update the state this way need to firebase
     fishes[key] = null;
-    this.setState({ fishes }); 
-  }
+    this.setState({ fishes });
+  };
 
   loadSamplesFishes = () => {
     this.setState({ fishes: sampleFishes });
-  }
+  };
 
-  addToOrder = (key) => {
+  addToOrder = key => {
     const order = { ...this.state.order };
     order[key] = order[key] + 1 || 1;
     this.setState({
       order: order
-    })
+    });
+  };
+
+  removeFromOrder = key => {
+    const order = { ...this.state.order };
+    delete order[key];
+    this.setState({
+      order: order
+    });
   }
 
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header tagline='Fresh Daily'/>
+          <Header tagline="Fresh Daily" />
           <ul className="fishes">
-            {Object.keys(this.state.fishes).map(key =>
+            {Object.keys(this.state.fishes).map(key => (
               <Fish
-              key={key}
-              index={key}
-              details={this.state.fishes[key]}
-              addToOrder={this.addToOrder}/>
-            )}
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}
           </ul>
         </div>
-        <Order order={this.state.order} fishes={this.state.fishes}/>
+        <Order
+          order={this.state.order}
+          fishes={this.state.fishes}
+          removeFromOrder={this.removeFromOrder}
+        />
         <Inventory
           addFish={this.addFish}
           updateFish={this.updateFish}
@@ -96,8 +109,8 @@ class App extends React.Component {
           fishes={this.state.fishes}
         />
       </div>
-    )
-  } 
+    );
+  }
 }
 
 export default App;
--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -2,23 +2,28 @@ import React from 'react';
 import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
-  renderOrders = (key) => {
+  renderOrders = key => {
     const fish = this.props.fishes[key];
     const count = this.props.order[key];
-    const isAvailable = fish && fish.status.available === 'available'
-    // make sure the fish is loaded 
-    if(!fish) return null
+    const isAvailable = fish && fish.status === 'available';
+    // make sure the fish is loaded
+    if (!fish) return null;
 
-    if (isAvailable) {
-      return <li key={key}>Sorry {fish ? fish.name : 'fish'} is no longer available</li>
+    if (!isAvailable) {
+      return (
+        <li key={key}>
+          Sorry {fish ? fish.name : 'fish'} is no longer available
+        </li>
+      );
     }
     return (
       <li key={key}>
         {count} lbs {fish.name}
         {formatPrice(count * fish.price)}
+        <button onClick={() => this.props.removeFromOrder(key)}>&times;</button>
       </li>
-    )
-  }
+    );
+  };
   render() {
     const orderId = Object.keys(this.props.order);
     const total = orderId.reduce((prevTotal, key) => {
@@ -26,21 +31,21 @@ class Order extends React.Component {
       const count = this.props.order[key];
       const isAvailable = fish && fish.status === 'available';
       if (isAvailable) {
-        return prevTotal + (count * fish.price);
+        return prevTotal + count * fish.price;
       }
       return prevTotal;
     }, 0);
     return (
       <div className="order-wrap">
         <h2>Orders</h2>
-        <ul>{orderId.map(this.renderOrders)}</ul>
+        <ul className="order">{orderId.map(this.renderOrders)}</ul>
         <div className="total">
-          Total: 
+          Total:
           <strong>{formatPrice(total)}</strong>
         </div>
-      </div> 
-    )
-  } 
+      </div>
+    );
+  }
 }
 
 export default Order;
